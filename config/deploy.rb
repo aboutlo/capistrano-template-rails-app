bec8a2732143adf9e4ac417a7b6440dba1bfda1c
Lorenzo Sicilia <lorenzo.sicilia@gmail.com>
2014-11-06 00:29:42 +0000

    Adding more generic names

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,9 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'finsight'
-# set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'myapp' #app must be configured on server
 
-set :repo_url, "git@bitbucket.org:aboutlo/finsight.io"
+set :repo_url, "git@github.com:aboutlo/capistrano-template-rails-app.git"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
